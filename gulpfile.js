4ce0aed0fac5a3b2e9fb7e0622999b5dafc4f8d7
Vinícius M. Knob <knob.vinicius@gmail.com>
2019-10-30 11:06:21 -0300

    Divide files to concat with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 const { src, dest, series } = require('gulp');
-const { fs } = require('fs');
+const del = require("del");
+const replace = require('gulp-replace');
+const { readFileSync } = require('fs');
+const htmlbeautify = require('gulp-html-beautify');
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const BOOTSTRAP = 'bootstrap-v43';
+
+function clean() {
+    return del(['dist/*']);
 }
 
-exports.default = defaultTask
+function build() {
+    return src('src/index.xhtml')
+        .pipe(replace('__b.skin.xhtml__', readFileSync(`src/b.skin.xhtml`, 'utf8')))
+        .pipe(replace('__import.css.html__', readFileSync(`src/${BOOTSTRAP}/import.css.html`, 'utf8')))
+        .pipe(replace('__container.html__', readFileSync(`src/${BOOTSTRAP}/container.html`, 'utf8')))
+        .pipe(replace('__import.js.html__', readFileSync(`src/${BOOTSTRAP}/import.js.html`, 'utf8')))
+        .pipe(dest('dist'));
+}
+
+function beautify() {
+    var options = {
+        "indent_size": 4,
+        "indent_with_tabs": false,
+        "max_preserve_newlines": 1,
+        "end_with_newline": true
+    };
+    return src('dist/*')
+        .pipe(htmlbeautify(options))
+        .pipe(dest('dist'));
+}
+
+// Export tasks
+exports.default = series(clean, build, beautify);
